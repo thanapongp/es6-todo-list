d0991ef875fdfe4d4a99a59f23e3efbf0de76a6b
Thanapong Prathumchat <tanapong21@gmail.com>
2019-09-07 17:22:54 +0700

    Adds auto-reload

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import '../main.css';
 import App from './app';
 
 document.addEventListener('DOMContentLoaded', () => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/js/main.js',
   mode: 'development',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'src', 'js')
+    path: path.resolve(__dirname, 'dist')
   }
 };
